b51f95fffece5eddce41fdde153dc64711aa84a3
ShineKami <shinekami@madys.net>
2015-11-21 14:15:20 +0200

    Update gulp file, add send_ftp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 //--Project settings--//
     //gulp load modules
     var gulp = require('gulp'),
+        gutil = require('gulp-util'),
+        ftp = require('gulp-ftp'),
         watch = require('gulp-watch'),
         prefixer = require('gulp-autoprefixer'),
         uglify = require('gulp-uglify'),
@@ -15,16 +17,29 @@
         rimraf = require('rimraf'),
         browserSync = require("browser-sync"),
         plumber = require('gulp-plumber'),
+        rename = require('gulp-rename'),
         reload = browserSync.reload;
+
+    //project setting
+    var settings = {
+        //ftp settings
+        ftp:{
+            host: '',
+            user: '',
+            pass: '',
+            remotePath: '/'
+        }
+    };
 
     //gulp set path
     var path = {
         //Path for build
         build: {
+            project: 'build/**/*.*', 
             html: 'build/',
             js: 'build/js/',
             css: 'build/css/',
-            img: 'build/imgs/',
+            imgs: 'build/imgs/',
             images: 'build/images/',
             fonts: 'build/fonts/',
         },
@@ -33,7 +48,7 @@
             html: 'src/template/*.html', 
             js: 'src/js/main.js',
             style: 'src/style/main.scss',
-            img: 'src/imgs/**/*.*',
+            imgs: 'src/imgs/**/*.*',
             images: 'src/images/**/*.*',
             fonts: 'src/fonts/**/*.*'
         },
@@ -42,7 +57,7 @@
             html: 'src/template/**/*.html',
             js: 'src/js/**/*.js',
             style: 'src/style/**/*.scss',
-            img: 'src/img/**/*.*',
+            imgs: 'src/imgs/**/*.*',
             images: 'src/images/**/*.*',
             fonts: 'src/fonts/**/*.*'
         },
@@ -54,7 +69,7 @@
         server: {
             baseDir: "./build"
         },
-        tunnel: true,
+        tunnel: false,
         host: 'localhost',
         port: 9000,
         logPrefix: "Frontend_Devil"
@@ -78,7 +93,11 @@
             .pipe(rigger())
             .pipe(sourcemaps.init())
             .pipe(uglify())
-            .pipe(sourcemaps.write())
+            .pipe(sourcemaps.write('../js', {
+                sourceMappingURL: function(file) {
+                    return file.relative + '.map';
+                }
+            }))
             .pipe(gulp.dest(path.build.js))
             .pipe(reload({stream: true}));
     });
@@ -91,14 +110,18 @@
             .pipe(sass({errLogToConsole: true}))
             .pipe(prefixer())
             .pipe(cssmin())
-            .pipe(sourcemaps.write())
+            .pipe(sourcemaps.write('../css', {
+                sourceMappingURL: function(file) {
+                    return file.relative + '.map';
+                }
+            }))
             .pipe(gulp.dest(path.build.css))
             .pipe(reload({stream: true}));
     });
 
     //Template images task
     gulp.task('imgs:build', function () {
-        gulp.src(path.src.img)
+        gulp.src(path.src.imgs)
             .pipe(plumber())
             .pipe(imagemin({
                 progressive: true,
@@ -106,7 +129,7 @@
                 use: [pngquant()],
                 interlaced: true
             }))
-            .pipe(gulp.dest(path.build.img))
+            .pipe(gulp.dest(path.build.imgs))
             .pipe(reload({stream: true}));
     });
 
@@ -138,10 +161,10 @@
         watch([path.watch.style], function(event, cb) {
             gulp.start('style:build');
         });
-        watch([path.watch.img], function(event, cb) {
+        watch([path.watch.imgs], function(event, cb) {
             gulp.start('imgs:build');
         });
-        watch([path.watch.img], function(event, cb) {
+        watch([path.watch.images], function(event, cb) {
             gulp.start('images:build');
         });
         watch([path.watch.js], function(event, cb) {
@@ -154,8 +177,20 @@
         browserSync(server_config);
     });
 
+    //Load project on ftp
+    gulp.task('send_ftp', function () {
+    return gulp.src(path.build.project)
+        .pipe(ftp({
+            host: settings.ftp.host,
+            user: settings.ftp.user,
+            pass: settings.ftp.pass,
+            remotePath: settings.ftp.remotePath
+        }))
+        .pipe(gutil.noop());
+    });
+
     //rm temp files
-    gulp.task('clean:build', function (cb) {
+    gulp.task('clean', function (cb) {
         rimraf(path.clean, cb);
     });
 //#################//
@@ -163,24 +198,24 @@
 //--Project builds--//
     //Dev build
     gulp.task('build-dev', [
-        'clean:build',
         'html:build',
         'js:build',
         'style:build',
         'fonts:build',
         'imgs:build',
-        'images:build'
+        'images:build',
+        'webserver', 
+        'watch'
     ]);
 
-    //Dest build
-    gulp.task('build', [
-        'html:build',
+    //js test
+    gulp.task('build-js', [
         'js:build',
-        'style:build',
-        'fonts:build',
-        'imgs:build',
-        'images:build'
+        'webserver',
+        'watch'
     ]);
+
+    //css test
 //##################//
 
-gulp.task('default', ['build-dev', 'webserver', 'watch']);
+gulp.task('default', ['build-dev']);
