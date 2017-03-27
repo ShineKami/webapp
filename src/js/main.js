66e7a16578cdd5b1e5092055b981b853aafbeb2f
Stanislav Vyshynskyi <shinekami@madys.net>
2017-03-27 19:49:51 +0300

    Add main.js WebApp template.

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,38 @@
  */
 @@include('../../bower_components/jquery/dist/jquery.js')
 
-
 /*
- * Custom
+ * Frontend WebApp
  */
+(function($) {
+	'use strict';
+
+	function WebApp(){
+		this.appname = "WebApp";
+		this.version = "1.0.0";
+		this.author = "ShineKami(https://madys.net/)";
+	}
+  
+  //Init example function
+  WebApp.prototype.example = function(){
+    console.log("Webapp work!");
+  }
+  
+	//Init all functional
+	WebApp.prototype.init = function(){
+		if(!$('body').hasClass('webapp-init')){
+			this.example();
+
+			$('body').addClass('webapp-init');
+		}
+	}
+
+	//Global fronted application
+	window.app = new WebApp;
+})(jQuery);
+
+jQuery(document).ready(function(){
+	app.init();
+}).ajaxComplete(function(){
+	app.init();
+})
